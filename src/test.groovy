866e71e3a69a97f54c6c3f57c3df6e6eb92a7cb0
bharath0080 <bharathforany@gmail.com>
2018-04-05 21:02:19 +0530

    Update test.groovy

---
+++ src/test.groovy
@@ -0,0 +1,26 @@
+def call(Map config) {
+	echo "${config.projectName}"
+	stage ('Cleanorkspace'){
+            // Clean workspace before doing anything
+            deleteDir()
+	}
+            try {
+                stage ('Clone') {
+                    //checkout scm
+                     sh "echo scm checkout"
+                    
+                }
+                stage ('Build') {
+                    sh "echo 'building ${config.projectName} ...'"
+                }
+                stage ('Tests') {
+					sh "echo testing"
+                }
+                stage ('Deploy') {
+                    sh "echo 'deploying to server ${config.serverDomain}...'"
+                }
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+  }
